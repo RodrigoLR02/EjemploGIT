b5f2cb2e8a8116c754c1d0d2f586e7502e11af8d
rodri <rodri@DESKTOP-IQT2RSK.home>
2023-11-28 17:28:49 +0100

    Primera semana de trabajo

--- PRUEBAGIT/src/main/java/Principal/principal.java
+++ PRUEBAGIT/src/main/java/Principal/principal.java
@@ -3,7 +3,10 @@ package Principal;
 public class principal {
 	
 	public static void main (String[] args) {
-		
+		System.out.println("Sisisi");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
 		
 	}
 
