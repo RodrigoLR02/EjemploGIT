6e01e8df3c24559c177312e2d4b8a4ae6ec53bb9
rodri <rodri@DESKTOP-IQT2RSK.home>
2023-11-28 17:23:14 +0100

    Interfaces y modelo

---
+++ PRUEBAGIT/src/main/java/Interfaces/MiInterfaz.java
@@ -0,0 +1,6 @@
+package Interfaces;
+
+public interface MiInterfaz {
+	
+	public int metododevuelveentero();
+}
---
+++ PRUEBAGIT/src/main/java/Modelo/MiClase.java
@@ -0,0 +1,13 @@
+package Modelo;
+
+import Interfaces.MiInterfaz;
+
+public class MiClase implements MiInterfaz {
+
+	@Override
+	public int metododevuelveentero() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
